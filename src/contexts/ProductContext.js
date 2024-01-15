ae7c04c33e78888c65955c356288da087634aea4
hassanrnt <hassanritouny97@gmail.com>
2024-01-15 16:09:05 +0100

    ecoomerce-Dona

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {createContext, useState,useEffect}from 'react';
+// create context
+export const ProductContext = createContext();
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+
+const ProductProvider= ({children}) => {
+  // product state
+  const[products, setProduct] = useState({});
+  // fetch product
+  useEffect(()=>{
+    const fetchProducts = async() =>{
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setProduct(data);
+    };
+    fetchProducts();
+  },[]);
+  return <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>;
 };
 
-export default ProductContext;
+export default ProductProvider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
+// import product context
 
 const Home = () => {
   return <div>Homepage</div>;
